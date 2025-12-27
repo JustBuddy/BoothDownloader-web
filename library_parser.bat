e93dc7dabaf99fcfa71290248134ff479db988c2
Buddy <8122264+JustBuddy@users.noreply.github.com>
2025-12-27 18:45:14 +0100

    Slight adjustments to the .bat script.

--- library_parser.bat
+++ library_parser.bat
@@ -1,2 +1,6 @@
+@echo off
+REM Simple batch runner for the library builder. Make sure to install dependencies.
+REM See readme for more details.
 python .\library_parser.py
-pause
+timeout /t 5
+exit
